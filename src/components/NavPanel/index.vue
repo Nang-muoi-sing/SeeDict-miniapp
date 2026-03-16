eba8b1d0963b214ee0f199dbd062bb40946e2f18
nan_cheng <nan_cheng@foxitsoftware.com>
2026-03-17 07:55:28 +0800

    feat: add nav bar

---
+++ src/components/NavPanel/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="flex h-full flex-row justify-between rounded-lg bg-white">
+    <view
+      class="hover:bg-wheat-50 text-rosybrown-600 hover:text-rosybrown-800 m-1 content-center rounded-xs px-3 py-1.5 font-bold transition-all ease-in-out"
+    >
+      <navigator url="/page/index/index" >主页</navigator>
+    </view>
+    <view
+      class="hover:bg-wheat-50 text-rosybrown-600 hover:text-rosybrown-800 m-1 content-center rounded-xs px-3 py-1.5 font-bold transition-all ease-in-out"
+    >
+      <navigator url="/page/index/index" >入门</navigator>
+    </view>
+    <view
+      class="hover:bg-wheat-50 text-rosybrown-600 hover:text-rosybrown-800 m-1 content-center rounded-xs px-3 py-1.5 font-bold transition-all ease-in-out"
+    >
+      <text
+        href="https://jcnf40n3hvft.feishu.cn/docx/FSqidtsgjo25x0x6R1KcChopnTc"
+        target="_blank"
+        >
+        帮助
+      </text>
+    </view>
+    <view
+      class="hover:bg-wheat-50 text-rosybrown-600 hover:text-rosybrown-800 m-1 content-center rounded-xs px-3 py-1.5 font-bold transition-all ease-in-out"
+    >
+      <navigator url="/page/index/index" >工具</navigator>
+    </view>
+    <view
+      class="hover:bg-wheat-50 text-rosybrown-600 hover:text-rosybrown-800 m-1 content-center rounded-xs px-3 py-1.5 font-bold transition-all ease-in-out"
+    >
+      <navigator url="/page/index/index" >关于</navigator>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,16 @@
 <template>
   <view class="bg-wheat-50 relative min-h-screen z-10 overflow-hidden home-page">
-    <view class="relative mx-auto mt-5 flex w-fit flex-col items-center space-y-8 text-center">
+    <view class="flex justify-center px-4 pt-8 md:justify-end md:px-16 md:pb-0">
+      <NavPanel class="relative z-30 w-xs"></NavPanel>
+    </view>
+    <view class="relative mx-auto flex w-fit flex-col items-center space-y-8 text-center">
       <view class="z-30 block pl-10 w-3xs"
         :style="{
           transform: `translateY(-${Math.min(scrollY * 1, 200)}px)`,
           opacity: Math.max(0, 1 - scrollY / 500),
         }"
       >
-        <image alt="" src="@/assets/logo-see.svg" />
+        <image alt="" src="@/assets/logo-see.svg" style="height: 400rpx" />
       </view>
     </view>
     <search-bar class="relative w-xs sm:w-sm md:w-md" :on-search="goSearch" />
@@ -21,6 +24,7 @@ import { onMounted, ref, onBeforeUnmount} from 'vue'
 import Footer from '@/components/Footer/index.vue';
 import SearchBar from '@/components/SearchBar/index.vue';
 import WordsDeck from '@/components/WordsDeck/index.vue'
+import NavPanel from '@/components/NavPanel/index.vue'
 import './index.styl'
 import Taro from "@tarojs/taro";
 
